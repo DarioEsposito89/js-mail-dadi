da5ecf2e2cfb22e1aa550cc46f132adf0db7f725
DarioEsposito89 <darioesposito.work@gmail.com>
2023-07-05 20:15:38 +0200

    Esercizio dadi e numeri completo

--- js/dice.js
+++ js/dice.js
@@ -12,29 +12,33 @@ btnGenerateDice.addEventListener("click", function () {
 
     if (userLaunch > pcLaunch) {
         console.log("l'utente ha vinto")
+        document.getElementById("result-dice").innerHTML = ("Il vincitore è l'UTENTE");
     } else if (userLaunch === pcLaunch) {
         console.log("l'utente e il pc hanno pareggiato")
+        document.getElementById("result-dice").innerHTML = ("Hai pareggiato");
     } else {
         console.log("il pc ha vinto")
+        document.getElementById("result-dice").innerHTML = ("Il vincitore è il Pc");
     }
 })
 
 const btnCancelDice = document.getElementById("cancel-dice");
 btnCancelDice.addEventListener("click", function () {
     // sparizione parametri Utente e Pc
-    document.getElementById("user-dice").value = "";
-    document.getElementById("pc-dice").value = "";
+    document.getElementById("user-dice").innerHTML = "";
+    document.getElementById("pc-dice").innerHTML = "";
+    document.getElementById("result-dice").innerHTML = "";
 })
 
-// Vince chi ha il numero più grande
+// Vince chi ha il numero più grande-------------------------------------------------------
 const btnGenerateNumber = document.getElementById("generate-number");
 btnGenerateNumber.addEventListener("click", function () {
         
     const userFirstNumber = document.getElementById("userfirstnumber").value;
-    console.log("L'utente ha inserito come primo numero", userFirstNumber);
+    console.log("L'utente ha inserito come primo numero", parseInt(userFirstNumber));
 
     const userSecondNumber = document.getElementById("usersecondnumber").value;
-    console.log("L'utente ha inserito come secondo numero", userSecondNumber);
+    console.log("L'utente ha inserito come secondo numero", parseInt(userSecondNumber));
 
     const userThirdNumber = document.getElementById("userthirdnumber").value;
     console.log("L'utente ha inserito come terzo numero:", parseInt(userThirdNumber));
@@ -56,22 +60,30 @@ btnGenerateNumber.addEventListener("click", function () {
     const sommaNumbersPc = pcNumber1 + pcNumber2 + pcNumber3;
     console.log("La somma dei numeri del Pc:", sommaNumbersPc);
 
-    document.getElementById("user-number").innerHTML = sommaNumbersPc;
+    document.getElementById("pc-number").innerHTML = sommaNumbersPc;
 
 
     if (sommaNumbersUser > sommaNumbersPc) {
         console.log("l'utente ha vinto")
+        document.getElementById("result-number").innerHTML = ("Il vincitore è l'UTENTE");
     } else if (sommaNumbersUser === sommaNumbersPc) {
         console.log("l'utente e il pc hanno pareggiato")
+        document.getElementById("result-number").innerHTML = ("Hai pareggiato");
     } else {
         console.log("il pc ha vinto")
+        document.getElementById("result-number").innerHTML = ("Il vincitore è il Pc");
     }
 })
 
 const btnCancelNumber = document.getElementById("cancel-number");
 btnCancelNumber.addEventListener("click", function () {
     // sparizione parametri Utente e Pc
-    document.getElementById("user-number").value = "";
-    document.getElementById("pc-number").value = "";
+    document.getElementById("userFirstNumber").value = "";
+    document.getElementById("userSecondNumber").value = "";
+    document.getElementById("userThirdNumber").value = "";
+
+    document.getElementById("user-number").innerHTML = "";
+    document.getElementById("pc-number").innerHTML = "";
+    document.getElementById("result-number").innerHTML = "";
 })
 
