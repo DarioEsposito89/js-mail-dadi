a4bf1d5aa78f3a0293a793da7a3572a63b9d04fb
DarioEsposito89 <darioesposito.work@gmail.com>
2023-07-06 10:51:53 +0200

    corretto esercizio

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
 const listaEmail = ["mariorossi@gmail.com", "giuseppeverdi@gmail.com", "vittoriobianchi@gmail.com"]
 console.log(listaEmail)
 
+const emailInseritaUtente = document.getElementById("email").value;
+
 let emailTrovata = false;
 
 const btnVerifica = document.getElementById("verifica");
 btnVerifica.addEventListener("click", function () {
     
+    emailTrovata = false;
     for (let i = 0; i < listaEmail.length; i++) {
         console.log(i);
-        listaEmail[i];
         console.log(listaEmail[i]);
-        let emailInseritaUtente = document.getElementById("email").value;
-        emailInseritaUtente === listaEmail[i];
-        if (emailInseritaUtente === listaEmail[i]) {
+        const currentEmail = listaEmail[i];
+        if (emailInseritaUtente === currentEmail) {
             emailTrovata === true;
-        }   else {
-            emailTrovata === false;
-        }
+        } 
+    }
 
-        if (emailTrovata === true) {
-            console.log("L'email inserita è presente nell'archivio");
-        } else {
-            console.log("L'email non è presente nell'archivio");
-        }
+    if (emailTrovata === true) {
+        console.log("L'email inserita è presente nell'archivio");
+        document.getElementById("risultato").innerHTML = ("L'email inserita è presente nell'archivio")
+    } else {
+        console.log("L'email NON è presente nell'archivio");
+        document.getElementById("risultato").innerHTML = ("L'email inserita NON è presente nell'archivio")
     }
 })
